a0ce4195496483dfec0371047cfff22b0f74f598
anupam <anupam.nitdgp14@gmail.com>
2023-09-05 23:22:52 +0530

    First Commit. With only web and lombock dependencies.

---
+++ src/main/java/com/example/spring5securityapp/Spring5SecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5SecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring5securityapp/Spring5SecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5SecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
